991a25677ac0f3d12aa50e55f50476ba1d2f6880
Relapse42 <rem630@hotmail.com>
2017-01-08 21:44:07 +0100

    Commit simple
    truc a gadeau de moi

--- src/Employe/AutoPartBundle/Controller/DefaultController.php
+++ src/Employe/AutoPartBundle/Controller/DefaultController.php
@@ -12,35 +12,24 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
-
 class DefaultController extends Controller
 {
     /**
-     * @Route("/tutu")
+     * @Route("/")
      */
     public function indexAction(Request $request)
     {
         $lesStationChoice=null;
-        $libelle= array();
-        $code= array();
-
+        $lesTypesVoituresChoice=null;
         $lesStations = $this->get("app.requete_employe")->getLesStations();
         $lesTypesVoitures = $this->get("app.requete_employe")->getLesTypesVoitures();
-        //var_dump($lesStations);die;
-
+        $formBuilder = $this->get('form.factory')->createBuilder();
         foreach ( $lesStations as $station ) {
             $lesStationChoice[$station["nom"]]=$station["id"];
-
         }
-        //var_dump($lesStationChoice);die;
-        foreach ( $lesTypesVoitures as $type ) {
-            $libelle[]=$type['libelle'];
+        foreach ( $lesTypesVoitures as $voiture ) {
+            $lesTypesVoituresChoice[$voiture["libelle"]]=$voiture["code"];
         }
-        foreach ( $lesTypesVoitures as $c ) {
-            $code[]=$c['code'];
-        }
-        $formBuilder = $this->get('form.factory')->createBuilder();
-
 
             $formBuilder
                 ->add('etatvoiture', TextType::class)
@@ -49,20 +38,17 @@ class DefaultController extends Controller
                 ->add('nbkilometres', TextType::class)
                 ->add('numcartegrise', TextType::class)
                 ->add("idstation", ChoiceType::class,
-                        array(
-                            'choices'  =>
-                                $lesStationChoice
-                            ,
+                    array(
+                        "attr" => array("class" => "form-control select2"),
+                        'choices'  => $lesStationChoice,
                     ))
                 ->add('codevoiture', ChoiceType::class,
                     array(
                         "attr" => array("class" => "form-control select2"),
-                        'choices' => [$libelle],
-                        'choice_label' => function ($value, $key, $index) {
-                            return $value;
-                        },
-
+                        'choices'  =>   $lesTypesVoituresChoice,
                     ))
+
+
                 ->add('submit', SubmitType::class);
 
 
@@ -71,8 +57,8 @@ class DefaultController extends Controller
         $form->handleRequest($request);
         if ($form->isSubmitted()) {
             $data = $form->getData();
-            var_dump($data);die;
-           // $result = $this->get("app.requete_employe")->ajoutVehicule($data);
+
+            $result = $this->get("app.requete_employe")->ajoutVehicule($data);
            var_dump($data);die;
             return $this->redirectToRoute('task_success');
         }
