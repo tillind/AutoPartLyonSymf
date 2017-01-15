cbf76b224cbc1e4d400fdfa82548fb9e83587102
Relapse42 <rem630@hotmail.com>
2017-01-15 01:01:23 +0100

    Modification Véhicule

--- src/Employe/AutoPartBundle/Business/RequeteBdd.php
+++ src/Employe/AutoPartBundle/Business/RequeteBdd.php
@@ -28,16 +28,19 @@ class RequeteBdd
         }
         return $lesStations;
     }
-    public function getLesTypesVoitures(){
-        $lesTypesVoitures =array();
-
+    public function getDateAsString($var,$var2,$var3){
+        $date=[];
         if($this->connexion instanceof \PDO){
-            $stmt = $this->connexion->query("SELECT code,libelle FROM typevoiture")->fetchAll();
-            foreach($stmt as $leType){
-                $lesTypesVoitures[]= array('code'=>$leType[0],'libelle'=>$leType[1]);
+            $stmt = $this->connexion->prepare("SELECT to_char($var,'DD-MM-YYYY'),to_char($var2,'DD-MM-YYYY')FROM voiture WHERE datedebutassurance= :var2");
+            $stmt->bindParam(":var2",$var3);
+            $stmt->execute();
+            foreach($stmt as $laDate){
+                $date= array('date'=>$laDate[0],'date2'=>$laDate[1]);
             }
-        }
-        return $lesTypesVoitures;
+        };
+
+
+        return $date;
     }
 
 
@@ -46,13 +49,13 @@ class RequeteBdd
         $lesStations=null;
         $result=null;
         if($this->connexion instanceof \PDO){
-            $stmt = $this->connexion->prepare("SELECT idvoiture, etatvoiture, datedebutassurance, datefinassurance, nbkilometre, numcartegrise,  voiture.idstation, codetypevoiture, nomvoiture,station.nom FROM voiture INNER JOIN station ON voiture.idstation=station.idstation WHERE idvoiture= :var");
+            $stmt = $this->connexion->prepare("SELECT idvoiture, etatvoiture, to_char(datedebutassurance,'DD-MM-YYYY'), to_char(datefinassurance,'DD-MM-YYYY'), nbkilometre, numcartegrise,  voiture.idstation, codetypevoiture, nomvoiture,station.nom FROM voiture INNER JOIN station ON voiture.idstation=station.idstation WHERE idvoiture= :var");
             $stmt->bindParam(":var",$var,\PDO::PARAM_STR);
             $stmt->execute();
 
             foreach($stmt as $lavoiture){
                 $lesVoiture[]= new Voiture($lavoiture[0],$lavoiture[1],$lavoiture[2],$lavoiture[3],$lavoiture[4],$lavoiture[5],$lavoiture[6],$lavoiture[7],$lavoiture[8]);
-                $lesStations[]=array('nom'=>$lavoiture[9]);
+                $lesStations[]=array('nom'=>$lavoiture[9],'id'=> $lavoiture[0]);
             }
         };
         return $result=array($lesVoiture,$lesStations);
@@ -88,10 +91,41 @@ class RequeteBdd
         if($this->connexion instanceof \PDO){
             $stmt = $this->connexion->query("SELECT code, libelle FROM typevoiture")->fetchAll();
             foreach($stmt as $uneCateg){
-                $lesCategories[]= array("cat" =>$uneCateg[0],"lib"=>$uneCateg[1]);
+                $lesCategories[$uneCateg[1]]= $uneCateg[0];
             }
+        }else{
+            //  echo"un probleme";die;
         }
         return $lesCategories;
     }
 
+    public function getLesVoituresByCateg($var){
+        $lesVoiture =array();
+
+        if($this->connexion instanceof \PDO){
+            $stmt = $this->connexion->prepare("SELECT idvoiture,etatvoiture,nomvoiture,idstation,libelle FROM voiture,typeVoiture WHERE codetypevoiture= :var and voiture.codetypevoiture=typevoiture.code");
+            $stmt->bindParam(":var",$var,\PDO::PARAM_STR);
+            $stmt->execute();
+
+            foreach($stmt as $lavoiture){
+                $lesVoiture[]= new Voiture($lavoiture[0],$lavoiture[2],$lavoiture[1],$lavoiture[3],$lavoiture[4]);
+            }
+        }else{
+            //  echo"un probleme";die;
+        }
+        return $lesVoiture;
+    }
+    public function modifVehicule($arrayVehicule){
+        $stmt = $this->connexion->prepare("UPDATE public.voiture SET etatvoiture=:etat, datedebutassurance=:datedeb, datefinassurance=:datefin, nbkilometre=:nbkilo, numcartegrise=:numcartegrise, proprietaire=TRUE, idstation=:idstation, codetypevoiture=:codetypevoiture, nomvoiture=:nomvoiture WHERE idvoiture=:idvoiture;");
+        $stmt->bindParam(':etat',$arrayVehicule['etatvoiture'],\PDO::PARAM_STR);
+        $stmt->bindParam(":datedeb",$arrayVehicule['datedebutassurance'],\PDO::PARAM_STR);
+        $stmt->bindParam(":datefin",$arrayVehicule['datefinassurance'],\PDO::PARAM_STR);
+        $stmt->bindParam(":nbkilo",$arrayVehicule['nbkilometres'],\PDO::PARAM_STR);
+        $stmt->bindParam(":numcartegrise",$arrayVehicule['numcartegrise'],\PDO::PARAM_STR);
+        $stmt->bindParam(":idstation",$arrayVehicule['idstation'],\PDO::PARAM_INT);
+        $stmt->bindParam(":codetypevoiture",$arrayVehicule['codevoiture'],\PDO::PARAM_STR);
+        $stmt->bindParam(":nomvoiture",$arrayVehicule['nomvehicule'],\PDO::PARAM_STR);
+        $stmt->bindParam(":idvoiture",$arrayVehicule['idvehicule'],\PDO::PARAM_INT);
+        $stmt->execute();
+    }
 }
--- src/Employe/AutoPartBundle/Controller/DefaultController.php
+++ src/Employe/AutoPartBundle/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace Employe\AutoPartBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\FormTypeInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
@@ -17,61 +18,242 @@ class DefaultController extends Controller
     /**
      * @Route("/")
      */
-    public function indexAction(Request $request)
+    public function indexAction()
     {
-        $lesStationChoice=null;
-        $lesTypesVoituresChoice=null;
+        if ($this->get('session')->isStarted()) {
+            $type = $this->get('session')->get('type');
+            if ($type == 'utilisateur') {
+                return $this->redirectToRoute('client_autopart_default_index');
+            } elseif ($type == 'employe') {
+                return $this->redirectToRoute('employe_autopart_default_index');
+            }
+        }
+        return $this->render('EmployeAutoPartBundle:Default:index.html.twig',
+            array()
+        );
+    }
 
-
-
-
-
+    /**
+     * @Route("/ajout")
+     */
+    public function ajoutAction(Request $request)
+    {
+        $lesStationChoice = null;
+        $lesTypesVoituresChoice = null;
         $lesStations = $this->get("app.requete_employe")->getLesStations();
-        $lesTypesVoitures = $this->get("app.requete_employe")->getLesTypesVoitures();
+        $lesCategories = $this->get("app.requete_employe")->getLesCategVoiture();
         $formBuilder = $this->get('form.factory')->createBuilder();
-        foreach ( $lesStations as $station ) {
-            $lesStationChoice[$station["nom"]]=$station["id"];
+        foreach ($lesStations as $station) {
+            $lesStationChoice[$station["nom"]] = $station["id"];
         }
-        foreach ( $lesTypesVoitures as $voiture ) {
-            $lesTypesVoituresChoice[$voiture["libelle"]]=$voiture["code"];
+        $formBuilder
+            ->add('etatvoiture', ChoiceType::class, array(
+                'choices'  => array(
+                    'Bon' => 'Bon',
+                    'En panne' => 'En panne',
+                    'A supprimer' => 'Supprimer',
+                ),
+            ))
+            ->add('datedebutassurance', TextType::class)
+            ->add('datefinassurance', TextType::class)
+            ->add('nbkilometres', TextType::class)
+            ->add('numcartegrise', TextType::class)
+            ->add("idstation", ChoiceType::class,
+                array(
+                    "attr" => array("class" => "form-control select2"),
+                    'choices' => $lesStationChoice,
+                ))
+            ->add('codevoiture', ChoiceType::class,
+                array(
+                    "attr" => array("class" => "form-control select2"),
+                    'choices' => $lesCategories,
+                ))
+            ->add('nomvehicule', TextType::class)
+            ->add('submit', SubmitType::class);
+
+
+        $form = $formBuilder->getForm();
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted()) {
+            $data = $form->getData();
+            $result = $this->get("app.requete_employe")->ajoutVehicule($data);
+            return $this->render('EmployeAutoPartBundle:Default:ajout.html.twig', array(
+                "mesStations" => $lesStations,
+                "mesVoitures" => $lesCategories,
+                'form' => $form->createView()
+            ));
         }
 
+        return $this->render('EmployeAutoPartBundle:Default:ajout.html.twig', array(
+            "mesStations" => $lesStations,
+            "mesVoitures" => $lesCategories,
+            'form' => $form->createView()
+        ));
+    }
+
+    /**
+     * @Route("/recherche")
+     */
+    public function rechercheAction(Request $request)
+    {
+        // Récupération des catégories de voiture
+
+        $lesCategories = $this->get("app.requete_employe")->getLesCategVoiture();
+        $lesCategories["Pas de préférence"]=null; //valeur par défaut
+
+        //création du formulaire
+        $formBuilder = $this->get('form.factory')->createBuilder();
+        $formBuilder->add('categorie', ChoiceType::class, array(
+            'choices'  => $lesCategories,
+        ))
+            ->add('dateDebut',TextType::class,array('required' => false))
+            ->add('dateFin',TextType::class,array('required' => false))
+            ->add('submit',SubmitType::class);
+        $form = $formBuilder->getForm();
+        $form->handleRequest($request);
+
+        //TO DO: vérifier que dateFin> dateDebut
+        //TO DO: renvoyer vers la liste des voitures qui correspondent
+        //récupération des résultats du formulaire
+        $lesVoitures =array();
+        if ($form->isSubmitted()) {
+            $data = $form->getData();
+            $dateDebut= $data['dateDebut'];
+            $dateFin= $data['dateFin'];
+            $cat= $data['categorie'];
+            //tableau des résultats des requête pour les dates et la catégorie
+            $lesVoitures = $this->get("app.requete_employe")->getLesVoitureByDateAndCateg($dateDebut,$dateFin,$cat);
+        }
+
+        //affichage du formulaire
+        return $this->render('EmployeAutoPartBundle:Default:recherche.html.twig',
+            array(
+                'form' => $form->createView(),
+                'lesVoitures'=>$lesVoitures
+            ));
+
+    }
+    /**
+     * @Route("/voiture/{id}")
+     */
+    public function voitureByIdAction($id=null)
+    {
+        if($this->get('session')->isStarted()){
+            $type = $this->get('session')->get('type');
+            if ($type == 'utilisateur') {
+                return $this->redirectToRoute('client_autopart_default_index');
+            } elseif ($type == 'employe') {
+                return $this->redirectToRoute('employe_autopart_default_index');
+            }
+        }
+        $lesVoitures = $this->get("app.requete_employe")->getLesVoituresById($id);
+
+
+        return $this->render('EmployeAutoPartBundle:Default:ficheVehicule.html.twig',
+            array(
+                "mesVoitures"=>$lesVoitures,
+            ));
+    }
+    /**
+     * @Route("/modifVoiture/{id}")
+     */
+    public function modifVoitureByIdAction(Request $request,$id=null)
+    {
+        $lesStationChoice = null;
+        $lesTypesVoituresChoice = null;
+        $lesDonneesPreRemplies=null;
+        $dates=null;
+        $lesStations = $this->get("app.requete_employe")->getLesStations();
+        $lesTypesVoitures = $this->get("app.requete_employe")->getLesCategVoiture();
+        $lesDonneesPreRemplies = $this->get("app.requete_employe")->getLesVoituresById($id);
+        foreach ($lesStations as $station) {
+            $lesStationChoice[$station["nom"]] = $station["id"];
+        }
+
+        foreach ($lesDonneesPreRemplies[0] as $voiture) {
+            $dates = $this->get("app.requete_employe")->getDateAsString("datedebutassurance","datefinassurance",$voiture->getDatedebutassurance());
+        }
+        $formBuilder = $this->get('form.factory')->createBuilder();
+        foreach ($lesDonneesPreRemplies[0] as $voiture) {
             $formBuilder
-                ->add('etatvoiture', TextType::class)
-                ->add('datedebutassurance', TextType::class)
-                ->add('datefinassurance', TextType::class)
-                ->add('nbkilometres', TextType::class)
-                ->add('numcartegrise', TextType::class)
+                ->add('etatvoiture', ChoiceType::class, array(
+                    'choices'  => array(
+                        'Bon' => 'Bon',
+                        'En panne' => 'En panne',
+                        'A supprimer' => 'Supprimer',
+                    ),'data' => $voiture->getEtatVoiture()
+                ))
+                ->add('datedebutassurance', TextType::class,array('data' => $dates['date']))
+                ->add('datefinassurance', TextType::class,array('data' => $dates['date2']))
+                ->add('nbkilometres', TextType::class, array('data' => $voiture->getNbkilometre()))
+                ->add('numcartegrise', TextType::class,array('data' => $voiture->getNumcartegrise()))
                 ->add("idstation", ChoiceType::class,
                     array(
+                        'data' => $lesStationChoice[$lesDonneesPreRemplies[1][0]['nom']],
                         "attr" => array("class" => "form-control select2"),
-                        'choices'  => $lesStationChoice,
-                    ))
-                ->add('codevoiture', ChoiceType::class,
+                        'choices' => $lesStationChoice,
+
+                    ));
+                $formBuilder->add('codevoiture', ChoiceType::class,
                     array(
                         "attr" => array("class" => "form-control select2"),
-                        'choices'  =>   $lesTypesVoituresChoice,
+                        'choices' => $lesTypesVoitures,
+
                     ))
-
-
+                ->add('nomvehicule', TextType::class,array('data' => $voiture->getnomVoiture()))
+                ->add('idvehicule', TextType::class,array('data' => $voiture->getIdVoiture()))
                 ->add('submit', SubmitType::class);
 
-
-            $form = $formBuilder->getForm();
+        }
+        $form = $formBuilder->getForm();
 
         $form->handleRequest($request);
         if ($form->isSubmitted()) {
             $data = $form->getData();
 
-            $result = $this->get("app.requete_employe")->ajoutVehicule($data);
-           var_dump($data);die;
-            return $this->redirectToRoute('task_success');
+            $this->get("app.requete_employe")->modifVehicule($data);
+
+            $lesVoitures = $this->get("app.requete_employe")->getLesVoituresById($id);
+
+
+            return $this->render('EmployeAutoPartBundle:Default:ficheVehicule.html.twig',
+                array(
+                    "mesVoitures"=>$lesVoitures,
+                ));
         }
 
-        return $this->render('EmployeAutoPartBundle:Default:index.html.twig',array(
-            "mesStations"=>$lesStations,
-            "mesVoitures"=> $lesTypesVoitures,
+        return $this->render('EmployeAutoPartBundle:Default:modifVehicule.html.twig', array(
+            "mesStations" => $lesStations,
+            "mesVoitures" => $lesTypesVoitures,
+            "mesDonnesPreRemplies" => $lesDonneesPreRemplies,
+            "mesStationsChoisies" => $lesStationChoice,
             'form' => $form->createView()
         ));
     }
+    /**
+     * @Route("/catalogue/{id}")
+     */
+    public function catalogueByCategAction($id=null)
+    {
+        if($this->get('session')->isStarted()){
+            $type = $this->get('session')->get('type');
+            if ($type == 'utilisateur') {
+                return $this->redirectToRoute('client_autopart_default_index');
+            } elseif ($type == 'employe') {
+                return $this->redirectToRoute('employe_autopart_default_index');
+            }
+        }
+        $lesVoitures = $this->get("app.requete_base")->getLesVoituresByCateg($id);
+        $lesCateg = $this->get("app.requete_base")->getLesCategVoiture();
+        return $this->render('EmployeAutoPartBundle:Default:catalogue.html.twig',
+            array(
+                "mesVoitures"=>$lesVoitures,
+                "lesCategs"=>$lesCateg
+            )
+        );
+    }
 }
+
+
+
